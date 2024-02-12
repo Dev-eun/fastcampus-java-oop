2230af6c1f0aea7943c6189a857e2342a2f93543
박하은 <deveun23@gmail.com>
2024-02-12 18:39:54 +0900

    Design #3 - Sort interface
    인터페이스를 설계하고 각 구현체가 인터페이스를 구현하는 구조로 수정

--- src/main/java/com/fastcampus/javaoop/logic/BubbleSort.java
+++ src/main/java/com/fastcampus/javaoop/logic/BubbleSort.java
@@ -3,7 +3,8 @@ package com.fastcampus.javaoop.logic;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BubbleSort <T extends Comparable<T>> {
+public class BubbleSort <T extends Comparable<T>> implements Sort<T> {
+    @Override
     public List<T> sort(List<T> list) {
         // 입력과 출력의 메모리를 구분 -> 출력에 변화를 줘도 입력에 영향이 가지 않음
         List<T> output = new ArrayList<>(list);
